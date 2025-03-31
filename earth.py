eefe78bb432a5b57f46189b0ee6d0639e9e39145
Renata-fernandez <rlfernandez@mail.austral.edu.ar>
2025-03-31 15:40:29 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,7 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
+    res1=(y>x)
+    res2=(y<x)
+    print(f"The result of {x} comes first in the dictionary than {y} is {res1}")
+    print(f"The result of {y} comes first in the dictionary than {x} is {res2}")
